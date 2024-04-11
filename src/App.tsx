ebde9e0acbab9aa8b05c3c6295e1056bcea2202f
Lobov.EM <lobov.em@gmail.com>
2024-04-11 12:52:10 +0300

    build(Project):  start new project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import reactLogo from './assets/react.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <p>Hello world</p>
+      <img src={reactLogo}></img>
+    </>
+  );
+}
+
+export default App;
