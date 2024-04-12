aed1c96c61206807cd82001c8b0838d61802b6b5
Lobov.EM <lobov.em@gmail.com>
2024-04-12 21:43:25 +0300

    feat(Orders): add layout to components of orders in header

--- src/components/MainSection/MainSection.tsx
+++ src/components/MainSection/MainSection.tsx
@@ -2,6 +2,7 @@ import { Col, Container, Row } from 'react-bootstrap';
 import './MainSection.scss';
 import { FC } from 'react';
 import { NavigationMenu } from '../NavigationMenu/NavigationMenu';
+import { Orders } from '../Orders/Orders';
 
 export const MainSection: FC = () => {
   return (
@@ -10,7 +11,9 @@ export const MainSection: FC = () => {
         <Col lg={1} className="leftSide">
           <NavigationMenu />
         </Col>
-        <Col lg={11}>2 of 2</Col>
+        <Col lg={11}>
+          <Orders />
+        </Col>
       </Row>
     </Container>
   );
--- src/components/Orders/Orders.tsx
+++ src/components/Orders/Orders.tsx
@@ -1,5 +1,19 @@
+import { Button, ListGroup } from 'react-bootstrap';
+import { FC } from 'react';
 import './Orders.scss';
 
-export const Orders = () => {
-  return <div>Orders</div>;
+export const Orders: FC = () => {
+  return (
+    <div className="orders">
+      <div className="orders__header">
+        <Button className="rounded-circle orders__button button">+</Button>
+        <p className="orders__headerTitle">Orders / 25</p>
+      </div>
+      <ListGroup>
+        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+      </ListGroup>
+    </div>
+  );
 };
