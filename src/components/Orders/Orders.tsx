1703f194866b34976c8536797da3ea661dee2fae
Lobov.EM <lobov.em@gmail.com>
2024-04-13 10:56:35 +0300

    feat(Orders): add layout and styles to components of detailOrder

--- src/components/Orders/Orders.tsx
+++ src/components/Orders/Orders.tsx
@@ -1,7 +1,8 @@
-import { Button, Col, Container, Image, Row } from 'react-bootstrap';
+import { Button, CloseButton, Image } from 'react-bootstrap';
 import { FC } from 'react';
 import iconList from '../../assets/iconList.png';
 import iconTrush from '../../assets/iconTrush.png';
+import imgMonitor from '../../assets/monitor.png';
 import './Orders.scss';
 
 export const Orders: FC = () => {
@@ -12,12 +13,197 @@ export const Orders: FC = () => {
         <p className="orders__headerTitle">Orders / 25</p>
       </div>
 
-      <Container fluid className="orders__listItem listItem">
-        <Row className="align-items-center listItem__item">
-          <Col md={5}>
-            <p className="listItem__title">Long long title name very long order</p>
+      <div className="orders__listItem listItem">
+        <div className="listItem__itemWrap">
+          <div className="align-items-center listItem__item">
+            <div className="listItem__titleWrap">
+              <p className="listItem__title">
+                Long long title name very long order long title name very long order{' '}
+              </p>
+            </div>
+
+            <div className="listItem__countWrap">
+              <Button variant="white" className="listItem__iconListWrap">
+                <Image src={iconList} className="listItem__iconList" />
+              </Button>
+
+              <div>
+                <p className="listItem__countProducts">23</p>
+                <p className="listItem__countProductsDesc">Products</p>
+              </div>
+            </div>
+
+            <div className="listItem__dateWrap">
+              <p className="listItem__dateShort">04 / 12</p>
+              <p className="listItem__dateLarge">06/ Apr / 2024</p>
+            </div>
+
+            <div className="listItem__priceWrap">
+              <p className="listItem__priceShort">2500 $</p>
+              <p className="listItem__priceLarge">250 000 UAH</p>
+            </div>
+
+            <div className="listItem__iconTrush iconTrush">
+              <Button variant="white">
+                <Image src={iconTrush} className="iconTrush__icon" />
+              </Button>
+            </div>
+          </div>
+
+          <div className="align-items-center listItem__item">
+            <div className="listItem__titleWrap">
+              <p className="listItem__title">
+                Long long title name very long order long title name very long order{' '}
+              </p>
+            </div>
+
+            <div className="listItem__countWrap">
+              <Button variant="white" className="listItem__iconListWrap">
+                <Image src={iconList} className="listItem__iconList" />
+              </Button>
+
+              <div>
+                <p className="listItem__countProducts">23</p>
+                <p className="listItem__countProductsDesc">Products</p>
+              </div>
+            </div>
+
+            <div className="listItem__dateWrap">
+              <p className="listItem__dateShort">04 / 12</p>
+              <p className="listItem__dateLarge">06/ Apr / 2024</p>
+            </div>
+
+            <div className="listItem__priceWrap">
+              <p className="listItem__priceShort">2500 $</p>
+              <p className="listItem__priceLarge">250 000 UAH</p>
+            </div>
+
+            <div className="listItem__iconTrush iconTrush">
+              <Button variant="white">
+                <Image src={iconTrush} className="iconTrush__icon" />
+              </Button>
+            </div>
+          </div>
+
+          <div className="align-items-center listItem__item">
+            <div className="listItem__titleWrap">
+              <p className="listItem__title">
+                Long long title name very long order long title name very long order{' '}
+              </p>
+            </div>
+
+            <div className="listItem__countWrap">
+              <Button variant="white" className="listItem__iconListWrap">
+                <Image src={iconList} className="listItem__iconList" />
+              </Button>
+
+              <div>
+                <p className="listItem__countProducts">23</p>
+                <p className="listItem__countProductsDesc">Products</p>
+              </div>
+            </div>
+
+            <div className="listItem__dateWrap">
+              <p className="listItem__dateShort">04 / 12</p>
+              <p className="listItem__dateLarge">06/ Apr / 2024</p>
+            </div>
+
+            <div className="listItem__priceWrap">
+              <p className="listItem__priceShort">2500 $</p>
+              <p className="listItem__priceLarge">250 000 UAH</p>
+            </div>
+
+            <div className="listItem__iconTrush iconTrush">
+              <Button variant="white">
+                <Image src={iconTrush} className="iconTrush__icon" />
+              </Button>
+            </div>
+          </div>
+        </div>
+
+        {/* <div className="orders__detailWrap detailOrder">
+          <div className="detailOrder__btnCloseWrap">
+            <CloseButton className="detailOrder__btnClose" />
+          </div>
+
+          <p className="detailOrder__title">
+            Long long title name very long order long title name very long order
+          </p>
+
+          <div className="detailOrder__buttonWrap">
+            <Button className="rounded-circle button">+</Button>
+            <p>Add product</p>
+          </div>
+
+          <div className="detailOrder__listItem item">
+            <div className="item__status item__status_sm"></div>
+            <Image className="item__img" src={imgMonitor} />
+
+            <div className="item__titleWrap">
+              <p className="item__title">
+                Samsung 49-inch Odyssey G9 Curved Gaming Monitor with QLED
+              </p>
+              <p className="item__titleDesc">SN- 9876543210</p>
+            </div>
+
+            <p className="item__status">Free</p>
+
+            <div className="item__iconTrush iconTrush">
+              <Button variant="white">
+                <Image src={iconTrush} className="iconTrush__icon" />
+              </Button>
+            </div>
+          </div>
+
+          <div className="detailOrder__listItem item">
+            <div className="item__status item__status_sm"></div>
+            <Image className="item__img" src={imgMonitor} />
+
+            <div className="item__titleWrap">
+              <p className="item__title">
+                Samsung 49-inch Odyssey G9 Curved Gaming Monitor with QLED
+              </p>
+              <p className="item__titleDesc">SN- 9876543210</p>
+            </div>
+
+            <p className="item__status">Free</p>
+
+            <div className="item__iconTrush iconTrush">
+              <Button variant="white">
+                <Image src={iconTrush} className="iconTrush__icon" />
+              </Button>
+            </div>
+          </div>
+
+          <div className="detailOrder__listItem item">
+            <div className="item__status item__status_sm"></div>
+            <Image className="item__img" src={imgMonitor} />
+
+            <div className="item__titleWrap">
+              <p className="item__title">
+                Samsung 49-inch Odyssey G9 Curved Gaming Monitor with QLED
+              </p>
+              <p className="item__titleDesc">SN- 9876543210</p>
+            </div>
+
+            <p className="item__status">Free</p>
+
+            <div className="item__iconTrush iconTrush">
+              <Button variant="white">
+                <Image src={iconTrush} className="iconTrush__icon" />
+              </Button>
+            </div>
+          </div>
+        </div> */}
+
+        {/* <Row className="align-items-center listItem__item">
+          <Col md={5} className="listItem__titleWrap">
+            <p className="listItem__title">
+              Long long title name very long order long title name very long order{' '}
+            </p>
           </Col>
-          <Col md={2} className="listItem__count">
+
+          <Col md={2} className="listItem__countWrap">
             <Button variant="white" className="listItem__iconListWrap">
               <Image src={iconList} className="listItem__iconList" />
             </Button>
@@ -28,17 +214,17 @@ export const Orders: FC = () => {
             </div>
           </Col>
 
-          <Col md={2} className="listItem__date">
+          <Col md={2} className="listItem__dateWrap">
             <p className="listItem__dateShort">04 / 12</p>
             <p className="listItem__dateLarge">06/ Apr / 2024</p>
           </Col>
 
-          <Col md={2}>
+          <Col md={2} className="listItem__priceWrap">
             <p className="listItem__priceShort">2500 $</p>
             <p className="listItem__priceLarge">250 000 UAH</p>
           </Col>
 
-          <Col md={1}>
+          <Col md={1} className="listItem__iconTrushWrap">
             <Button variant="white">
               <Image src={iconTrush} className="listItem__iconTrush" />
             </Button>
@@ -46,10 +232,13 @@ export const Orders: FC = () => {
         </Row>
 
         <Row className="align-items-center listItem__item">
-          <Col md={5}>
-            <p className="listItem__title">Длинное предлинное длиннючее название order</p>
+          <Col md={5} className="listItem__titleWrap">
+            <p className="listItem__title">
+              Long long title name very long order long title name very long order{' '}
+            </p>
           </Col>
-          <Col md={2} className="listItem__count">
+
+          <Col md={2} className="listItem__countWrap">
             <Button variant="white" className="listItem__iconListWrap">
               <Image src={iconList} className="listItem__iconList" />
             </Button>
@@ -60,54 +249,23 @@ export const Orders: FC = () => {
             </div>
           </Col>
 
-          <Col md={2} className="listItem__date">
+          <Col md={2} className="listItem__dateWrap">
             <p className="listItem__dateShort">04 / 12</p>
             <p className="listItem__dateLarge">06/ Apr / 2024</p>
           </Col>
 
-          <Col md={2}>
+          <Col md={2} className="listItem__priceWrap">
             <p className="listItem__priceShort">2500 $</p>
             <p className="listItem__priceLarge">250 000 UAH</p>
           </Col>
 
-          <Col md={1}>
+          <Col md={1} className="listItem__iconTrushWrap">
             <Button variant="white">
               <Image src={iconTrush} className="listItem__iconTrush" />
             </Button>
           </Col>
-        </Row>
-        <Row className="align-items-center listItem__item">
-          <Col md={5}>
-            <p className="listItem__title">Длинное предлинное длиннючее название order</p>
-          </Col>
-          <Col md={2} className="listItem__count">
-            <Button variant="white" className="listItem__iconListWrap">
-              <Image src={iconList} className="listItem__iconList" />
-            </Button>
-
-            <div>
-              <p className="listItem__countProducts">23</p>
-              <p className="listItem__countProductsDesc">Products</p>
-            </div>
-          </Col>
-
-          <Col md={2} className="listItem__date">
-            <p className="listItem__dateShort">04 / 12</p>
-            <p className="listItem__dateLarge">06/ Apr / 2024</p>
-          </Col>
-
-          <Col md={2}>
-            <p className="listItem__priceShort">2500 $</p>
-            <p className="listItem__priceLarge">250 000 UAH</p>
-          </Col>
-
-          <Col md={1}>
-            <Button variant="white">
-              <Image src={iconTrush} className="listItem__iconTrush" />
-            </Button>
-          </Col>
-        </Row>
-      </Container>
+        </Row>  */}
+      </div>
     </div>
   );
 };
