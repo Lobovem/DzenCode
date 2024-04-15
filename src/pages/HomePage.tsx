f516a26c270b689d2076165318ff178cbdf8693c
Lobov.EM <lobov.em@gmail.com>
2024-04-15 15:20:00 +0300

    feat(Redux): add RTK and mock data

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,24 @@
 import { FC } from 'react';
+import { RootState } from '../store/store';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment } from '../store/slices';
 
 export const HomePage: FC = () => {
-  return <h2>Home page</h2>;
+  const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h2>Home page</h2>
+      <button aria-label="Decrement value" onClick={() => dispatch(increment())}>
+        Increment
+      </button>
+
+      <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
+        Decrement
+      </button>
+
+      <h3>{count}</h3>
+    </div>
+  );
 };
