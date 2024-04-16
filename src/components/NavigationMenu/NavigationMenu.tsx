f5a8ecce9c719155ca0f5cb75c3b87cbd9c071cc
Lobov.EM <lobov.em@gmail.com>
2024-04-17 00:27:29 +0300

    fix(Router): fix bug with router of order when click link of order should show all fields order component

--- src/components/NavigationMenu/NavigationMenu.tsx
+++ src/components/NavigationMenu/NavigationMenu.tsx
@@ -3,8 +3,13 @@ import { User } from '../User/User';
 import { Nav } from 'react-bootstrap';
 import './NavigationMenu.scss';
 import { NavLink } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../../store/store';
+import { isDetailOrder } from '../../store/slices';
 
 export const NavigationMenu: FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
+
   return (
     <div className="navigationMenu">
       <User />
@@ -23,7 +28,11 @@ export const NavigationMenu: FC = () => {
 
         <Nav.Item className="navBar__item">
           <NavLink
-            to="/orders"
+            to="/orders" //TODO check out it
+            /*функция dispatch нужна, чтобы когда выбран роутер orders скрывался компонент detailOrder и 
+            вновь были показаны все поля order компонента, потому что при открытии dateilOrder 
+            скрываются некоторые поля order component*/
+            onClick={() => dispatch(isDetailOrder())}
             className={({ isActive }) =>
               isActive ? 'navBar__link navBar__link_active' : 'navBar__link'
             }
--- src/store/slices.ts
+++ src/store/slices.ts
@@ -69,6 +69,7 @@ export const dzenCodeSlice = createSlice({
     })
 
     builder.addCase(fetchProductListAndOrderList.fulfilled, (state, action) => {
+      state.isDetailOrder = false
       state.orderListWithProduct = action.payload
     })
 
